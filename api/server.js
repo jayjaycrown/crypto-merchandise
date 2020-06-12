6db11e9d05b0b06ad1c486153a6c404326bfd131
kizito917 <johnkingsley917@gmail.com>
2020-06-12 12:08:34 +0100

    user authentication done

---
+++ api/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import bodyparser from 'body-parser';
+import userAuthRoute from './routes/users/auth';
+var dotenv = require('dotenv').config();
+var app = express();
+var port = process.env.LOCAL_PORT || process.env.port
+
+
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use('/auth', userAuthRoute)
+app.use(cors(corsOptions));
+
+app.listen(port, process.env.IP, () => {
+	console.log(`Crypto-merchantile currently listening to port ${port}`);
+})
+
